523559c3cf49dc74dbf58bfa47a530276a01b96c
paucablop <pau.cabaneros@gmail.com>
2023-04-09 22:59:09 +0200

    add airpls

--- chemotools/baseline/air_pls.py
+++ chemotools/baseline/air_pls.py
@@ -10,13 +10,13 @@ logger = logging.getLogger(__name__)
 class AirPls(BaseEstimator, TransformerMixin):
     def __init__(
         self,
-        nr_iterations: int,
+        nr_iterations: int = 15,
         lam: int = 1e2,
-        differences: int = 2,
+        polynomial_order: int = 2,
     ):
         self.nr_iterations = nr_iterations
         self.lam = lam
-        self.differences = differences
+        self.polynomial_order = polynomial_order
 
     def fit(self, X: np.ndarray, y=None) -> "AirPls":
         # Check that X is a 2D array and has only finite values
@@ -42,17 +42,17 @@ class AirPls(BaseEstimator, TransformerMixin):
         if X_.shape[1] != self.n_features_in_:
             raise ValueError(f"Expected {self.n_features_in_} features but got {X_.shape[1]}")
 
-        # Calculate the whittaker smooth
+        # Calculate the air pls smooth
         for i, x in enumerate(X_):
-            X_[i] = self._calculate_air_pls(x)
+            X_[i] = x - self._calculate_air_pls(x)
 
         return X_.reshape(-1, 1) if X_.ndim == 1 else X_
 
     def _calculate_whittaker_smooth(self, x, w):
         x = np.asarray(x)
         n = len(x)
-        D = np.diff(np.eye(n), self.differences)
-        for i in range(3):
+        D = np.diff(np.eye(n), self.polynomial_order)
+        for i in range(self.polynomial_order):
             W = np.diag(w) + 1e-8*np.eye(n)
             Z = W + self.lam * np.dot(D, D.T)
             z = np.linalg.solve(Z, w * x)
@@ -63,7 +63,7 @@ class AirPls(BaseEstimator, TransformerMixin):
         m = x.shape[0]
         w = np.ones(m)
 
-        for i in range(self.nr_iterations):
+        for i in range(1, self.nr_iterations):
             z = self._calculate_whittaker_smooth(x, w)
             d = x - z
             dssn = np.abs(d[d<0].sum())          
@@ -72,7 +72,11 @@ class AirPls(BaseEstimator, TransformerMixin):
 
             w[d>=0]=0
             w[d<0] =  np.exp(i * np.abs(d[d < 0]) / dssn)
-            w[0] = np.exp(i * (d[d < 0]).max() / dssn)
+
+            negative_d = d[d < 0]
+            if negative_d.size > 0:
+                w[0] = np.exp(i * negative_d.max() / dssn)
+
             w[-1] = w[0]
 
         return z
