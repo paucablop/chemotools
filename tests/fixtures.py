c2c53f807f4d8328b43219e7a4145fcfd3081958
paucablop <pau.cabaneros@gmail.com>
2023-04-09 23:38:40 +0200

    add test fixtures

---
+++ tests/fixtures.py
@@ -0,0 +1,22 @@
+import numpy as np
+import os
+import pytest
+
+
+test_directory = os.path.dirname(os.path.abspath(__file__))
+
+path_to_resources = os.path.join(test_directory, "resources")
+
+
+@pytest.fixture
+def spectrum() -> np.ndarray:
+    return [np.loadtxt(os.path.join(path_to_resources, "spectrum.csv"), delimiter=",").tolist()]
+
+
+@pytest.fixture
+def reference_airpls() -> np.ndarray:
+    return [
+        np.loadtxt(
+            os.path.join(path_to_resources, "reference_airpls.csv"), delimiter=","
+        ).tolist()
+    ]
