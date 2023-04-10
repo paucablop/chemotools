2df572af282142c3162d110a1ba6aee23b9a72aa
paucablop <pau.cabaneros@gmail.com>
2023-04-10 07:51:52 +0200

    test functionality in max norm

--- tests/test_functionality.py
+++ tests/test_functionality.py
@@ -14,6 +14,15 @@ def test_air_pls(spectrum, reference_airpls):
     assert np.allclose(spectrum_corrected[0], reference_airpls[0], atol=1e-8)
 
 
+def test_max_norm(spectrum):
+    from chemotools.normalize import MaxNorm
+
+    max_norm = MaxNorm()
+    spectrum_corrected = max_norm.fit_transform(spectrum)
+
+    assert np.allclose(spectrum_corrected[0], spectrum[0] / np.max(spectrum[0]), atol=1e-8)
+
+
 def test_whitakker_smooth(spectrum, reference_whitakker):
     from chemotools.baseline import AirPls
 
