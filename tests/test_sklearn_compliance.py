40c172d1e2ba585dd7222ad83e76a97365392abd
paucablop <pau.cabaneros@gmail.com>
2023-05-05 23:15:04 +0200

    format tests

--- tests/test_sklearn_compliance.py
+++ tests/test_sklearn_compliance.py
@@ -1,10 +1,24 @@
 from sklearn.utils.estimator_checks import check_estimator
 
-from chemotools.baseline import AirPls, ArPls, ConstantBaselineCorrection, CubicSplineCorrection, LinearCorrection, NonNegative, PolynomialCorrection, SubtractReference
+from chemotools.baseline import (
+    AirPls,
+    ArPls,
+    ConstantBaselineCorrection,
+    CubicSplineCorrection,
+    LinearCorrection,
+    NonNegative,
+    PolynomialCorrection,
+    SubtractReference,
+)
 from chemotools.derivative import NorrisWilliams, SavitzkyGolay
 from chemotools.scale import IndexScaler, MinMaxScaler, NormScaler
 from chemotools.scatter import MultiplicativeScatterCorrection, StandardNormalVariate
-from chemotools.smooth import MeanFilter, MedianFilter, SavitzkyGolayFilter, WhittakerSmooth
+from chemotools.smooth import (
+    MeanFilter,
+    MedianFilter,
+    SavitzkyGolayFilter,
+    WhittakerSmooth,
+)
 from chemotools.variable_selection import RangeCut
 
 from tests.fixtures import spectrum
@@ -17,12 +31,14 @@ def test_compliance_air_pls():
     # Act & Assert
     check_estimator(transformer)
 
+
 # ArPls
 def test_compliance_ar_pls():
     # Arrange
     transformer = ArPls()
     # Act & Assert
     check_estimator(transformer)
+
 
 # ConstantBaselineCorrection
 def test_compliance_constant_baseline_correction():
@@ -31,12 +47,14 @@ def test_compliance_constant_baseline_correction():
     # Act & Assert
     check_estimator(transformer)
 
+
 # CubicSplineCorrection
 def test_compliance_cubic_spline_correction():
     # Arrange
     transformer = CubicSplineCorrection()
     # Act & Assert
     check_estimator(transformer)
+
 
 # IndexScaler
 def test_compliance_index_scaler():
@@ -45,12 +63,14 @@ def test_compliance_index_scaler():
     # Act & Assert
     check_estimator(transformer)
 
+
 # LinearCorrection
 def test_compliance_linear_correction():
     # Arrange
     transformer = LinearCorrection()
     # Act & Assert
     check_estimator(transformer)
+
 
 # LNormalize
 def test_compliance_l_norm():
@@ -59,12 +79,14 @@ def test_compliance_l_norm():
     # Act & Assert
     check_estimator(transformer)
 
+
 # MeanFilter
 def test_compliance_mean_filter():
     # Arrange
     transformer = MeanFilter()
     # Act & Assert
     check_estimator(transformer)
+
 
 # MedianFilter
 def test_compliance_median_filter():
@@ -73,12 +95,14 @@ def test_compliance_median_filter():
     # Act & Assert
     check_estimator(transformer)
 
+
 # MinMaxNormalize
 def test_compliance_min_max_norm():
     # Arrange
     transformer = MinMaxScaler()
     # Act & Assert
     check_estimator(transformer)
+
 
 # MultiplicativeScatterCorrection
 def test_compliance_multiplicative_scatter_correction():
@@ -87,6 +111,7 @@ def test_compliance_multiplicative_scatter_correction():
     # Act & Assert
     check_estimator(transformer)
 
+
 # NonNegative
 def test_compliance_non_negative():
     # Arrange
@@ -94,12 +119,14 @@ def test_compliance_non_negative():
     # Act & Assert
     check_estimator(transformer)
 
+
 # NorrisWilliams
 def test_compliance_norris_williams():
     # Arrange
     transformer = NorrisWilliams()
     # Act & Assert
     check_estimator(transformer)
+
 
 # NorrisWilliams
 def test_compliance_norris_williams_2():
@@ -116,12 +143,14 @@ def test_compliance_polynomial_correction():
     # Act & Assert
     check_estimator(transformer)
 
+
 # SavitzkyGolay
 def test_compliance_savitzky_golay():
     # Arrange
     transformer = SavitzkyGolay()
     # Act & Assert
     check_estimator(transformer)
+
 
 # SavitzkyGolayFilter
 def test_compliance_savitzky_golay_filter():
@@ -130,12 +159,14 @@ def test_compliance_savitzky_golay_filter():
     # Act & Assert
     check_estimator(transformer)
 
+
 # StandardNormalVariate
 def test_compliance_standard_normal_variate():
     # Arrange
     transformer = StandardNormalVariate()
     # Act & Assert
     check_estimator(transformer)
+
 
 # RangeCut
 def test_compliance_range_cut():
@@ -144,12 +175,14 @@ def test_compliance_range_cut():
     # Act & Assert
     check_estimator(transformer)
 
+
 # SubtractReference
 def test_compliance_subtract_reference():
     # Arrange
     transformer = SubtractReference()
     # Act & Assert
     check_estimator(transformer)
+
 
 # WhittakerSmooth
 def test_compliance_whittaker_smooth():
