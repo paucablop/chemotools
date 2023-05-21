1865f4ef4056c1d99c585d043b97dfce070058d2
paucablop <pau.cabaneros@gmail.com>
2023-05-21 14:27:45 +0200

    add docstrings to subtract reference

--- chemotools/baseline/subtract_reference.py
+++ chemotools/baseline/subtract_reference.py
@@ -6,6 +6,34 @@ from chemotools.utils.check_inputs import check_input
 
 
 class SubtractReference(OneToOneFeatureMixin, BaseEstimator, TransformerMixin):
+    """
+    A transformer that subtracts a reference spectrum from the input data.
+
+    Parameters
+    ----------
+    reference : np.ndarray, optional
+        The reference spectrum to subtract from the input data. If None, the original spectrum
+        is returned.
+
+    Attributes
+    ----------
+    n_features_in_ : int
+        The number of features in the input data.
+
+    _is_fitted : bool
+        Whether the transformer has been fitted to data.
+
+    Methods
+    -------
+    fit(X, y=None)
+        Fit the transformer to the input data.
+
+    transform(X, y=0, copy=True)
+        Transform the input data by subtracting the reference spectrum.
+
+    _subtract_reference(x)
+        Subtract the reference spectrum from a single spectrum.
+    """
     def __init__(
         self,
         reference: np.ndarray = None,
@@ -13,6 +41,22 @@ class SubtractReference(OneToOneFeatureMixin, BaseEstimator, TransformerMixin):
         self.reference = reference
 
     def fit(self, X: np.ndarray, y=None) -> "SubtractReference":
+        """
+        Fit the transformer to the input data.
+
+        Parameters
+        ----------
+        X : np.ndarray
+            The input data to fit the transformer to.
+
+        y : None
+            Ignored.
+
+        Returns
+        -------
+        self : SubtractReference
+            The fitted transformer.
+        """
         # Check that X is a 2D array and has only finite values
         X = check_input(X)
 
@@ -31,6 +75,22 @@ class SubtractReference(OneToOneFeatureMixin, BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X: np.ndarray, y=None) -> np.ndarray:
+        """
+        Transform the input data by subtracting the reference spectrum.
+
+        Parameters
+        ----------
+        X : np.ndarray
+            The input data to transform.
+
+        y : None
+            Ignored.
+
+        Returns
+        -------
+        X_ : np.ndarray
+            The transformed data.
+        """
         # Check that the estimator is fitted
         check_is_fitted(self, "_is_fitted")
 
@@ -51,4 +111,12 @@ class SubtractReference(OneToOneFeatureMixin, BaseEstimator, TransformerMixin):
         return X_.reshape(-1, 1) if X_.ndim == 1 else X_
 
     def _subtract_reference(self, x) -> np.ndarray:
+        """
+        Subtract the reference spectrum from a single spectrum.
+
+        Parameters
+        ----------
+        x : np.ndarray
+            The spectrum to subtract the reference from.
+        """
         return x - self.reference_
