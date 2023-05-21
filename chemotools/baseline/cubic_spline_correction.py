880a8dbe03e2362ef9ca3ef5f571094bf4fb0420
paucablop <pau.cabaneros@gmail.com>
2023-05-21 09:48:37 +0200

    add docstrings to cubic splines

--- chemotools/baseline/cubic_spline_correction.py
+++ chemotools/baseline/cubic_spline_correction.py
@@ -6,10 +6,52 @@ from sklearn.utils.validation import check_is_fitted
 from chemotools.utils.check_inputs import check_input
 
 class CubicSplineCorrection(OneToOneFeatureMixin, BaseEstimator, TransformerMixin):
+    """
+    A transformer that corrects a baseline by subtracting a cubic spline through the 
+    points defined by the indices.
+
+    Parameters
+    ----------
+    indices : list, optional
+        The indices of the features to use for the baseline correction.
+
+    Attributes
+    ----------
+    n_features_in_ : int
+        The number of features in the input data.
+
+    _is_fitted : bool
+        Whether the transformer has been fitted to data.
+
+    Methods
+    -------
+    fit(X, y=None)
+        Fit the transformer to the input data.
+
+    transform(X, y=0, copy=True)
+        Transform the input data by subtracting the constant baseline value.
+
+    """
     def __init__(self, indices: list = None) -> None:
         self.indices = indices
 
     def fit(self, X: np.ndarray, y=None) -> "CubicSplineCorrection":
+        """
+        Fit the transformer to the input data.
+
+        Parameters
+        ----------
+        X : np.ndarray
+            The input data to fit the transformer to.
+
+        y : None
+            Ignored.
+
+        Returns
+        -------
+        self : ConstantBaselineCorrection
+            The fitted transformer.
+        """
         # Check that X is a 2D array and has only finite values
         X = check_input(X)
 
@@ -27,6 +69,25 @@ class CubicSplineCorrection(OneToOneFeatureMixin, BaseEstimator, TransformerMixi
         return self
 
     def transform(self, X: np.ndarray, y=None, copy=True):
+        """
+        Transform the input data by subtracting the baseline.
+
+        Parameters
+        ----------
+        X : np.ndarray
+            The input data to transform.
+
+        y : None
+            Ignored.
+
+        copy : bool, optional
+            Whether to copy the input data or not.
+
+        Returns
+        -------
+        X_ : np.ndarray
+            The transformed data.
+        """
         # Check that the estimator is fitted
         check_is_fitted(self, "_is_fitted")
 
