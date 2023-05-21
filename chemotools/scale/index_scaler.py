1c9152bf5db9a678cea8560510b336d2f82f286c
paucablop <pau.cabaneros@gmail.com>
2023-05-21 16:07:34 +0200

    add docstrings to index scaler

--- chemotools/scale/index_scaler.py
+++ chemotools/scale/index_scaler.py
@@ -6,11 +6,51 @@ from chemotools.utils.check_inputs import check_input
 
 
 class IndexScaler(OneToOneFeatureMixin, BaseEstimator, TransformerMixin):
+    """
+    A transformer that scales the input data by the value at a given index.
+
+    Parameters
+    ----------
+    index : int, optional
+        The index to scale the data by.
+
+    Attributes
+    ----------
+    n_features_in_ : int
+        The number of features in the input data.
+
+    _is_fitted : bool
+        Whether the transformer has been fitted to data.
+
+    Methods
+    -------
+    fit(X, y=None)
+        Fit the transformer to the input data.
+
+    transform(X, y=0, copy=True)
+        Transform the input data by scaling by the value at a given index.
+    """
     def __init__(self, index: int = 0):
         self.index = index
 
 
     def fit(self, X: np.ndarray, y=None) -> "IndexScaler":
+        """
+        Fit the transformer to the input data.
+
+        Parameters
+        ----------
+        X : np.ndarray
+            The input data to fit the transformer to.
+
+        y : None
+            Ignored.
+
+        Returns
+        -------
+        self : IndexScaler
+            The fitted transformer.
+        """
         # Check that X is a 2D array and has only finite values
         X = check_input(X)
 
@@ -23,6 +63,22 @@ class IndexScaler(OneToOneFeatureMixin, BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X: np.ndarray, y=None) -> np.ndarray:
+        """
+        Transform the input data by scaling by the value at a given index.
+
+        Parameters
+        ----------
+        X : np.ndarray
+            The input data to transform.
+
+        y : None
+            Ignored.
+
+        Returns
+        -------
+        X_ : np.ndarray
+            The transformed data.
+        """
         # Check that the estimator is fitted
         check_is_fitted(self, "_is_fitted")
 
