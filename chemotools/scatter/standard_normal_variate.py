037e682f883fc1736e00dd22f11bcd63d81e5e91
paucablop <pau.cabaneros@gmail.com>
2023-05-21 15:35:19 +0200

    add docstrings to standard normal variate

--- chemotools/scatter/standard_normal_variate.py
+++ chemotools/scatter/standard_normal_variate.py
@@ -6,7 +6,42 @@ from chemotools.utils.check_inputs import check_input
 
 
 class StandardNormalVariate(OneToOneFeatureMixin, BaseEstimator, TransformerMixin):
+    """
+    A transformer that calculates the standard normal variate of the input data.
+
+    Attributes
+    ----------
+    n_features_in_ : int
+        The number of features in the input data.
+
+    _is_fitted : bool
+        Whether the transformer has been fitted to data.
+
+    Methods
+    -------
+    fit(X, y=None)
+        Fit the transformer to the input data.
+
+    transform(X, y=0, copy=True)
+        Transform the input data by calculating the standard normal variate.
+    """
     def fit(self, X: np.ndarray, y=None) -> "StandardNormalVariate":
+        """
+        Fit the transformer to the input data.
+        
+        Parameters
+        ----------
+        X : np.ndarray
+            The input data to fit the transformer to.
+
+        y : None
+            Ignored.
+
+        Returns
+        -------
+        self : StandardNormalVariate
+            The fitted transformer.
+        """
         # Check that X is a 2D array and has only finite values
         X = check_input(X)
 
@@ -19,6 +54,22 @@ class StandardNormalVariate(OneToOneFeatureMixin, BaseEstimator, TransformerMixi
         return self
 
     def transform(self, X: np.ndarray, y=None) -> np.ndarray:
+        """
+        Transform the input data by calculating the standard normal variate.
+
+        Parameters
+        ----------
+        X : np.ndarray
+            The input data to transform.
+
+        y : None
+            Ignored.
+
+        Returns
+        -------
+        X_ : np.ndarray
+            The transformed data.
+        """
         # Check that the estimator is fitted
         check_is_fitted(self, "_is_fitted")
 
