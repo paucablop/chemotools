da549b15ceb10b96b15d8131a9c4a1f3594fb853
paucablop <pau.cabaneros@gmail.com>
2023-05-21 16:10:45 +0200

    add docstrings to min max scaler

--- chemotools/scale/min_max_scaler.py
+++ chemotools/scale/min_max_scaler.py
@@ -6,11 +6,52 @@ from chemotools.utils.check_inputs import check_input
 
 
 class MinMaxScaler(OneToOneFeatureMixin, BaseEstimator, TransformerMixin):
+    """
+    A transformer that scales the input data by the maximum value or minimum 
+    value in the spectrum.
+
+    Parameters
+    ----------
+    norm : str, optional
+        The normalization to use. Can be "max" or "min". Default is "max".
+
+    Attributes
+    ----------
+    n_features_in_ : int
+        The number of features in the input data.
+
+    _is_fitted : bool
+        Whether the transformer has been fitted to data.
+
+    Methods
+    -------
+    fit(X, y=None)
+        Fit the transformer to the input data.
+
+    transform(X, y=0, copy=True)
+        Transform the input data by scaling by the maximum value.
+    """
     def __init__(self, norm: str = 'max'):
         self.norm = norm
 
 
     def fit(self, X: np.ndarray, y=None) -> "MinMaxScaler":
+        """
+        Fit the transformer to the input data.
+
+        Parameters
+        ----------
+        X : np.ndarray
+            The input data to fit the transformer to.
+
+        y : None
+            Ignored.
+
+        Returns
+        -------
+        self : MinMaxScaler
+            The fitted transformer.
+        """
         # Check that X is a 2D array and has only finite values
         X = check_input(X)
 
@@ -23,6 +64,22 @@ class MinMaxScaler(OneToOneFeatureMixin, BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X: np.ndarray, y=None) -> np.ndarray:
+        """
+        Transform the input data by scaling by the maximum or minimum value.
+
+        Parameters
+        ----------
+        X : np.ndarray
+            The input data to transform.
+
+        y : None
+            Ignored.
+
+        Returns
+        -------
+        X_ : np.ndarray
+            The transformed data.
+        """
         # Check that the estimator is fitted
         check_is_fitted(self, "_is_fitted")
 
