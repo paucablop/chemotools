38fc5863dd6a06ab5c89a0289bbcf0cc62e4fe9d
paucablop <pau.cabaneros@gmail.com>
2023-11-22 07:21:27 +0100

    update init for linear correction

--- chemotools/baseline/__init__.py
+++ chemotools/baseline/__init__.py
@@ -2,7 +2,7 @@ from ._air_pls import AirPls
 from ._ar_pls import ArPls
 from ._constant_baseline_correction import ConstantBaselineCorrection
 from ._cubic_spline_correction import CubicSplineCorrection
-from .linear_correction import LinearCorrection
+from ._linear_correction import LinearCorrection
 from .non_negative import NonNegative
 from .polynomial_correction import PolynomialCorrection
 from .subtract_reference import SubtractReference
