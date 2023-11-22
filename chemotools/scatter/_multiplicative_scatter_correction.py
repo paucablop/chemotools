94411cece11621157ddacc6a3cfbe47635e039f4
paucablop <pau.cabaneros@gmail.com>
2023-11-22 07:44:32 +0100

    update msc

--- chemotools/scatter/_multiplicative_scatter_correction.py
+++ chemotools/scatter/_multiplicative_scatter_correction.py
@@ -68,13 +68,7 @@ class MultiplicativeScatterCorrection(
             The fitted transformer.
         """
         # Check that X is a 2D array and has only finite values
-        X = check_input(X)
-
-        # Set the number of features
-        self.n_features_in_ = X.shape[1]
-
-        # Set the fitted attribute to True
-        self._is_fitted = True
+        X = self._validate_data(X)
 
         # Check that the length of the reference is the same as the number of features
         if self.reference is not None:
@@ -129,7 +123,7 @@ class MultiplicativeScatterCorrection(
             The transformed data.
         """
         # Check that the estimator is fitted
-        check_is_fitted(self, "_is_fitted")
+        check_is_fitted(self, "n_features_in_")
 
         # Check that X is a 2D array and has only finite values
         X = check_input(X)
