2766b04158cb17a13f2ffed2cdcd00efcd30a26a
paucablop <pau.cabaneros@gmail.com>
2023-11-22 07:39:30 +0100

    fix point scaler

--- chemotools/scale/_point_scaler.py
+++ chemotools/scale/_point_scaler.py
@@ -7,12 +7,12 @@ from chemotools.utils.check_inputs import check_input
 
 class PointScaler(OneToOneFeatureMixin, BaseEstimator, TransformerMixin):
     """
-    A transformer that scales the input data by the intensity value at a given point. 
+    A transformer that scales the input data by the intensity value at a given point.
     The point can be specified by an index or by a wavenumber.
 
     Parameters
     ----------
-    point : int, 
+    point : int,
         The point to scale the data by. It can be an index or a wavenumber.
 
     wavenumber : array-like, optional
@@ -25,12 +25,6 @@ class PointScaler(OneToOneFeatureMixin, BaseEstimator, TransformerMixin):
     point_index_ : int
         The index of the point to scale the data by. It is 0 if the wavenumbers are not provided.
 
-    n_features_in_ : int
-        The number of features in the input data.
-
-    _is_fitted : bool
-        Whether the transformer has been fitted to data.
-
     Methods
     -------
     fit(X, y=None)
@@ -39,10 +33,10 @@ class PointScaler(OneToOneFeatureMixin, BaseEstimator, TransformerMixin):
     transform(X, y=0, copy=True)
         Transform the input data by scaling by the value at a given Point.
     """
+
     def __init__(self, point: int = 0, wavenumbers: np.ndarray = None):
         self.point = point
         self.wavenumbers = wavenumbers
-
 
     def fit(self, X: np.ndarray, y=None) -> "PointScaler":
         """
@@ -62,20 +56,13 @@ class PointScaler(OneToOneFeatureMixin, BaseEstimator, TransformerMixin):
             The fitted transformer.
         """
         # Check that X is a 2D array and has only finite values
-        X = check_input(X)
-
-        # Set the number of features
-        self.n_features_in_ = X.shape[1]
-
-        # Set the fitted attribute to True
-        self._is_fitted = True
+        X = self._validate_data(X)
 
         # Set the point index
         if self.wavenumbers is None:
             self.point_index_ = self.point
         else:
             self.point_index_ = self._find_index(self.point)
-
 
         return self
 
@@ -97,7 +84,7 @@ class PointScaler(OneToOneFeatureMixin, BaseEstimator, TransformerMixin):
             The transformed data.
         """
         # Check that the estimator is fitted
-        check_is_fitted(self, "_is_fitted")
+        check_is_fitted(self, "point_index_")
 
         # Check that X is a 2D array and has only finite values
         X = check_input(X)
@@ -105,14 +92,16 @@ class PointScaler(OneToOneFeatureMixin, BaseEstimator, TransformerMixin):
 
         # Check that the number of features is the same as the fitted data
         if X_.shape[1] != self.n_features_in_:
-            raise ValueError(f"Expected {self.n_features_in_} features but got {X_.shape[1]}")
+            raise ValueError(
+                f"Expected {self.n_features_in_} features but got {X_.shape[1]}"
+            )
 
         # Scale the data by Point
         for i, x in enumerate(X_):
             X_[i] = x / x[self.point_index_]
-        
+
         return X_.reshape(-1, 1) if X_.ndim == 1 else X_
-    
+
     def _find_index(self, target: float) -> int:
         wavenumbers = np.array(self.wavenumbers)
         return np.argmin(np.abs(wavenumbers - target))
