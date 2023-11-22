b37be232eb9564448686e5d4c0dfdace8f2fed20
paucablop <pau.cabaneros@gmail.com>
2023-11-22 07:53:10 +0100

    update savgol filter

--- chemotools/smooth/__init__.py
+++ chemotools/smooth/__init__.py
@@ -1,4 +1,4 @@
 from ._mean_filter import MeanFilter
 from ._median_filter import MedianFilter
-from .savitzky_golay_filter import SavitzkyGolayFilter
+from ._savitzky_golay_filter import SavitzkyGolayFilter
 from .whittaker_smooth import WhittakerSmooth
