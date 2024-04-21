064afcfa69b9a2cd3ed86a2d2cc4e5e70a866a7a
MothNik <nik.zoe@web.de>
2024-04-21 13:58:41 +0200

    refactor: implemented fast computation of both squared forward finite differences matrix version that does not rely on `scipy.sparse` anymore to avoid overhead and redundant computations

--- chemotools/utils/finite_differences.py
+++ chemotools/utils/finite_differences.py
@@ -2,9 +2,6 @@ from math import comb
 from numbers import Integral
 
 import numpy as np
-from scipy.sparse import csr_matrix, dia_matrix
-from scipy.sparse import diags as spdiags
-from scipy.sparse import eye as speye
 from sklearn.utils import check_scalar
 
 
@@ -12,7 +9,8 @@ def calc_forward_diff_kernel(
     *,
     differences: int,
 ) -> np.ndarray:
-    """Computes the kernel for forward finite differences which can be applied to a
+    """
+    Computes the kernel for forward finite differences which can be applied to a
     series by means of a convolution, e.g.,
 
     ```python
@@ -27,18 +25,18 @@ def calc_forward_diff_kernel(
         The order of the differences starting from 0 for the original curve, 1 for the
         first order, 2 for the second order, ..., and ``m`` for the ``m``-th order
         differences.
-        Values below 0 are not allowed.
+        Values below 1 are not allowed.
 
     Returns
     -------
-    fin_diff_kernel : np.ndarray of shape (differences + 1,)
+    fin_diff_kernel : ndarray of shape (differences + 1,)
         A NumPy-1D-vector resembling the kernel from the code example above. To avoid
         loss of precision, the data type is ``np.int64``.
 
     Raises
     ------
     ValueError
-        If the difference order is below 0.
+        If the difference order is below 1.
 
     """
     # the input is validated
@@ -46,7 +44,7 @@ def calc_forward_diff_kernel(
         differences,
         name="differences",
         target_type=Integral,
-        min_val=0,
+        min_val=1,
         include_boundaries="left",
     )
 
@@ -60,192 +58,202 @@ def calc_forward_diff_kernel(
     )
 
 
-def forward_finite_diff_conv_matrix(
+def _gen_squ_fw_fin_diff_mat_cho_banded_transp_first(
     *,
+    n_data: int,
     differences: int,
-    series_size: int,
-) -> dia_matrix:
-    """Computes the convolution matrix for forward finite differences which can be
-    applied to a series by means of a matrix multiplication, e.g.,
+) -> np.ndarray:
+    """
+    Generates the squared forward finite differences matrix ``D.T @ D`` from the
+    forward finite difference matrix ``D`` of order ``differences``. It will be cast to
+    to the upper banded storage format used for LAPACK's banded Cholesky decomposition.
 
-    ```python
-        conv_mat = finite_diff_conv_matrix(differences=2, series_size=10)
-        differences = conv_mat @ series # boundaries require special care
-    ```
-
-    this is equivalent to
-
-    ```python
-        kernel = calc_forward_fin_diff_kernel(differences=2)
-        differences = np.convolve(series, np.flip(kernel), mode="valid")
-        # NOTE: NumPy flips the kernel internally due to the definition of convolution
-    ```
-
-    Parameters
-    ----------
-    differences : int
-        The order of the differences starting from 0 for the original curve, 1 for the
-        first order, 2 for the second order, ..., and ``m`` for the ``m``-th order
-        differences.
-        Values below 0 are not allowed.
-    series_size : int
-        The number of data points in the series to which the convolution matrix is
-        applied.
-
-    Returns
-    -------
-    diff_mat : dia_matrix of shape (series_size - differences, series_size)
-        A sparse matrix resembling the convolution matrix from the code example above.
-        To avoid loss of precision, the data type is ``np.int64``.
-
-    Raises
-    ------
-    ValueError
-        If the difference order is below 0, or ``series_size`` is not sufficient to
-        support the respective difference order.
+    All unused elements in the banded matrix are set to zero.
 
     """
 
-    # the input is validated
-    kernel_size = differences + 1
-    try:
-        check_scalar(
-            series_size,
-            name="n_features",  # for compatibility with sklearn
-            target_type=Integral,
-            min_val=kernel_size,
-            include_boundaries="left",
-        )
-    except ValueError:
-        raise ValueError(f"Got n_features = {series_size}, must be >= {kernel_size}.")
-
-    # afterwards, the kernel is computed ...
+    # the problems has to be split into a leading, a central, and a trailing part
+    # first, the leading part is computed because it might be that this is already
+    # enough
+    # first, the kernel for the forward differences is computed and the bandwidth is
+    # determined
     kernel = calc_forward_diff_kernel(differences=differences)
-    # ... and the convolution matrix is created
-    return spdiags(
-        diagonals=kernel,
-        offsets=np.arange(start=0, stop=kernel_size, step=1),  # type: ignore
-        shape=(series_size - kernel_size + 1, series_size),
-        format="dia",
-        dtype=np.int64,
+    n_bands = 1 + 2 * differences
+    lead_n_rows = min(kernel.size, n_data - kernel.size + 1)
+    lead_n_cols = kernel.size + lead_n_rows - 1
+
+    # the leading matrix is computed as a dense matrix
+    dtd = np.zeros(shape=(lead_n_rows, lead_n_cols), dtype=np.int64)
+    for row_idx in range(0, lead_n_rows):
+        dtd[row_idx, row_idx : row_idx + kernel.size] = kernel
+
+    # its squared form is computed
+    dtd = dtd.T @ dtd
+
+    # now, the leading matrix is converted to a banded matrix
+    dtd_banded = np.zeros(shape=(differences + 1, lead_n_cols), dtype=np.int64)
+    for diag_idx in range(0, differences + 1):
+        offset = differences - diag_idx
+        dtd_banded[diag_idx, offset:None] = np.diag(dtd, k=offset)
+
+    # if the number of data points is less than the kernel size minus one, the
+    # leading matrix is already the final matrix
+    if n_data <= n_bands:
+        return dtd_banded
+
+    # otherwise, a central part has to be inserted
+    # this turns out to be just a column-wise repetition of the kernel computed with
+    # double the difference order, so this matrix can simple be inserted into the
+    # computed leading D.T @ D matrix
+    # NOTE: the doubled kernel is the most central column of the banded D.T @ D already
+    #       computed
+    central_n_cols = n_data - dtd_banded.shape[1]
+    kernel_double = dtd_banded[::, kernel.size - 1].reshape((-1, 1))
+    return np.concatenate(
+        (
+            dtd_banded[::, 0 : kernel.size],
+            np.tile(kernel_double, (1, central_n_cols)),
+            dtd_banded[::, kernel.size :],
+        ),
+        axis=1,
     )
 
 
-def calc_limit_max_eigval_fin_diff_mat(differences: int) -> int:
-    """Computes the maximum eigenvalue of the forward finite difference matrix as
-    computed by ``forward_finite_diff_conv_matrix`` for the given difference order. It
-    only uses the limit value as the series size tends to infinity, but from some
-    tests, this seems to be an upper limit for the maximum singular value for any
-    series size which makes it ideal for thresholding.
+def _gen_squ_fw_fin_diff_mat_cho_banded_orig_first(
+    *,
+    n_data: int,
+    differences: int,
+) -> np.ndarray:
+    """
+    Generates the squared forward finite differences matrix ``D @ D.T`` from the
+    forward finite difference matrix ``D`` of order ``differences``. It will be cast to
+    to the upper banded storage format used for LAPACK's banded Cholesky decomposition.
 
-    Parameters
-    ----------
-    differences : int
-        The order of the differences starting from 0 for the original curve, 1 for the
-        first order, 2 for the second order, ..., and ``m`` for the ``m``-th order
-        differences.
-        Values below 0 are not allowed.
-
-    Returns
-    -------
-    max_eigval : int
-        The maximum eigenvalue of the forward finite difference matrix.
+    All unused elements in the banded matrix are set to zero.
 
     """
 
-    # NOTE: this was found rather empirically, but it works
-    return 4**differences
+    # this case is simpler than the transposed case because the matrix is just a
+    # Toeplitz matrix with the kernel of double the difference order on the diagonal
+    kernel_double = calc_forward_diff_kernel(differences=2 * differences)[
+        differences:None
+    ]
+    # for an odd difference order, the sign of the kernel has to be flipped
+    if differences % 2 == 1:
+        kernel_double = np.negative(kernel_double)
+
+    n_rows = n_data - kernel_double.size + 1
+    n_upp_plus_central_bands = min(n_rows, 1 + differences)
+
+    # the matrix is computed as a dense and simple filled by means of a loop
+    ddt_banded = np.zeros(shape=(n_upp_plus_central_bands, n_rows), dtype=np.int64)
+    main_diag_idx = min(differences, n_upp_plus_central_bands - 1)
+    for offset in range(0, n_upp_plus_central_bands):
+        ddt_banded[main_diag_idx - offset, offset:None] = kernel_double[offset]
+
+    return ddt_banded
 
 
-def posdef_mod_squared_fw_fin_diff_conv_matrix(
+def gen_squ_fw_fin_diff_mat_cho_banded(
     *,
-    fw_fin_diff_mat: dia_matrix,
+    n_data: int,
     differences: int,
-    dia_mod_matrix: dia_matrix | None,
-    max_eigval_mult: float,
-    dtype: type,
-) -> csr_matrix:
-    """Computes the modified squared forward finite difference matrix ``P`` for the
-    given difference order and series size. It is computed as
+    orig_first: bool,
+) -> np.ndarray:
+    """
+    Generates the squared forward finite differences matrix ``D @ D.T`` or ``D.T @ D``
+    from the forward finite difference matrix ``D`` of order ``differences``. It will be
+    cast to to the upper banded storage format used for LAPACK's banded Cholesky
+    decomposition.
 
-    ```python
-    # the pre-computation is obtained which might still be positive semi-definite
-    P = D.T @ M @ D
-    # the maximum eigenvalue of P is estimated to make it positive definite
-    max_lam_p = max_lam_dtd * max_lam_m
-    # by lifting the main diagonal, P is made numerical positive definite
-    P += max_lam_p * max_eigval_mult * I
-    ```
-
-    where ``D`` is the convolution matrix for forward finite differences, ``M`` is the
-    diagonal matrix of the modified weights, ``max_lam_dtd`` is the maximum eigenvalue
-    ``D.T @ D``, ``max_lam_m`` is the maximum eigenvalue of ``M``, i.e., the maximum
-    weight (since diagonal matrix), and ``max_lam_p`` is the maximum eigenvalue of
-    ``P``. For details on this approximation, please see Notes.
+    All unused elements in the banded matrix are set to zero.
 
     Parameters
     ----------
-    fw_fin_diff_mat : dia_matrix
-        The convolution matrix for forward finite differences resembling ``D`` from the
-        description above. It can be computed by ``forward_finite_diff_conv_matrix``.
+    n_data : int
+        The number of data points in the series to which the forward finite differences
+        are applied.
     differences : int
         The order of the differences starting from 0 for the original curve, 1 for the
         first order, 2 for the second order, ..., and ``m`` for the ``m``-th order
         differences.
-        Values below 0 are not allowed.
-    dia_mod_matrix : dia_matrix or None
-        The sparse diagonal matrix of the modification weights resembling ``M`` from the
-        description above. If ``None``, this multiplication is skipped.
-    max_eigval_mult : float
-        The multiple of the maximum eigenvalue of the modified squared forward finite
-        differences matrix that is added to the main diagonal of the output matrix to
-        make it positive definite according to the description above.
-    dtype : type
-        The data type of the output matrix.
+        Values below 1 are not allowed.
+    orig_first : bool
+        If ``True``, the squared forward finite differences matrix ``D @ D.T`` is
+        computed. Otherwise, the squared forward finite differences matrix ``D.T @ D``
+        is computed.
 
     Returns
     -------
-    posdef_squ_diff_mat : csr_matrix of shape (series_size - differences, series_size)
-        A positive definite sparse matrix resembling the squared forward finite
-        difference matrix ``P`` from the description above. It will be of data type
-        ``dtype``.
+    squ_fw_fin_diff_mat_cho_banded : ndarray of shape (n_bands, n_data - differences + 1) or (n_bands, n_data)
+        The squared forward finite differences matrix in the upper banded storage format
+        used for LAPACK's banded Cholesky decomposition (see Notes for details).
+        When ``orig_first`` is ``True``, the matrix has at maximum ``differences + 1``
+        bands (rows) and ``n_data - differences + 1`` columns.
+        Otherwise, the matrix has at maximum ``differences + 1`` bands (rows) and
+        ``n_data`` columns.
 
     Raises
     ------
     ValueError
-        If the difference order is below 0, or the number of grid points is not
-        sufficient to support the respective difference order.
+        If ``n_data`` is below ``differences + 1``, i.e., the kernel does not fit into
+        the data at least once.
+    ValueError
+        If the difference order is below 1.
 
     Notes
     -----
-    The approximation of ``max_lam_p`` is based on the spectral norm of the
-    matrix product. Since the spectral norm ``||P||2`` is submultiplicative, the
-    estimate ``||D.T||2 * ||M||2 * ||D||2`` is an upper bound for ``||P||2``.
-    As ``||D||2 = ||D.T||2 = sqrt(max_lam_dtd)`` and
-    ``||M||2 = sqrt(max_lam_m**2) = max_lam_m = M.max()``, the maximum eigenvalue of
-    ``P`` is estimated as ``max_lam_p = max_lam_dtd * max_lam_m``. For just ensuring
-    numerical stability, this is perfectly fine and it also won't overestimate the
-    maximum eigenvalue of ``P`` too much and therefore, the perturbation of the main
-    diagonal is kept small.
+    The squared forward finite differences matrix is stored in the upper banded storage
+    format used for LAPACK's banded Cholesky decomposition.
+    This upper diagonal ordered form is given by the following ordering
 
-    """
+    ```python
+    ab[u + i - j, j] == a[i,j]
+    ```
 
-    # first, the maximum eigenvalue of the finite difference matrix is computed
-    squ_diff_mat_eigval_max = calc_limit_max_eigval_fin_diff_mat(
-        differences=differences
+    e.g., for a symmetric matrix of shape (6, 6) with in total 3 superdiagonals,
+    3 subdiagonals, and the main diagonal, the ordering is as follows:
+
+    ```python
+    *   *   *   a03 a14 a25
+    *   *   a02 a13 a24 a35
+    *   a01 a12 a23 a34 a45 # ^ superdiagonals
+    a00 a11 a22 a33 a44 a55 # main diagonal
+    ```
+
+    where each `*` denotes a zero element.
+
+    Written out, this would give the following matrix:
+
+    ```python
+    a00 a01 a02 a03 0   0
+    a01 a11 a12 a13 a14 0
+    a02 a12 a22 a23 a24 a25
+    a03 a13 a23 a33 a34 a35
+    0   a14 a24 a34 a44 a45
+    0   0   a25 a35 a45 a55
+    ```
+
+    """  # noqa: E501
+
+    # first, it needs to be ensured that the number of data points is enough to
+    # support the kernel for the respective difference order at least once
+    check_scalar(
+        n_data,
+        name="n_data",
+        target_type=Integral,
+        min_val=differences + 1,
+        include_boundaries="left",
     )
 
-    # afterwards, the squared convolution matrix is computed
-    if dia_mod_matrix is None:
-        squ_diff_mat = fw_fin_diff_mat.T @ fw_fin_diff_mat
-        m_eigval_max = 1.0
-    else:
-        squ_diff_mat = fw_fin_diff_mat.T @ dia_mod_matrix @ fw_fin_diff_mat
-        m_eigval_max = dia_mod_matrix.data.max()
+    # afterwards, the squared forward finite differences matrix is computed
+    if orig_first:
+        return _gen_squ_fw_fin_diff_mat_cho_banded_orig_first(
+            n_data=n_data,
+            differences=differences,
+        )
 
-    # the main diagonal is lifted by a multiple of the machine epsilon
-    lift_mat = speye(m=fw_fin_diff_mat.shape[1], dtype=dtype, format="csr")
-    lift_mat *= max_eigval_mult * squ_diff_mat_eigval_max * m_eigval_max
-
-    # the positive definite matrix is returned
-    return squ_diff_mat + lift_mat
+    return _gen_squ_fw_fin_diff_mat_cho_banded_transp_first(
+        n_data=n_data,
+        differences=differences,
+    )
