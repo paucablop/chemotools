63b12172aa7a1430be683677dec2add534923319
MothNik <nik.zoe@web.de>
2024-05-11 19:35:52 +0200

    tests: added clearer comments to model tests

--- tests/test_for_utils/test_models.py
+++ tests/test_for_utils/test_models.py
@@ -20,11 +20,7 @@ _LambdaValueNumeric = Union[_Numeric, Tuple[_Numeric, _Numeric]]
 _LambdaValueNumericOrFlawed = Union[_LambdaValueNumeric, str]
 _WhittakerMethod = Union[str, models.WhittakerSmoothMethods]
 _WhittakerMethodSequence = List[_WhittakerMethod]
-_ExpectedLambdaResult = Union[
-    ExpectedWhittakerSmoothLambda,
-    Type[ValueError],
-    Type[TypeError],
-]
+_ExpectedLambdaResult = Union[ExpectedWhittakerSmoothLambda, Type[Exception]]
 _LambdaTestCombination = Tuple[
     _LambdaValueNumericOrFlawed,
     _WhittakerMethodSequence,
@@ -55,7 +51,7 @@ _all_whittaker_methods: _WhittakerMethodSequence = (
 @pytest.mark.parametrize(
     "combination",
     [
-        (  # Number 0
+        (  # Number 0 (fixed float; fixed method)
             100.0,
             _FIXED_WHITTAKER_METHODS,
             ExpectedWhittakerSmoothLambda(
@@ -66,7 +62,18 @@ _all_whittaker_methods: _WhittakerMethodSequence = (
                 log_auto_bounds=(_NAN, _NAN),
             ),
         ),
-        (  # Number 1
+        (  # Number 1 (fixed integer; fixed method)
+            100,
+            _FIXED_WHITTAKER_METHODS,
+            ExpectedWhittakerSmoothLambda(
+                fixed_lambda=100.0,
+                auto_bounds=(_NAN, _NAN),
+                fit_auto=False,
+                method_used=models.WhittakerSmoothMethods.FIXED,
+                log_auto_bounds=(_NAN, _NAN),
+            ),
+        ),
+        (  # Number 2 (coinciding floats; fixed method)
             (100.0, 100.0),
             _FIXED_WHITTAKER_METHODS,
             ExpectedWhittakerSmoothLambda(
@@ -77,7 +84,18 @@ _all_whittaker_methods: _WhittakerMethodSequence = (
                 log_auto_bounds=(_NAN, _NAN),
             ),
         ),
-        (  # Number 2
+        (  # Number 3 (coinciding integers; fixed method)
+            (100, 100),
+            _FIXED_WHITTAKER_METHODS,
+            ExpectedWhittakerSmoothLambda(
+                fixed_lambda=100.0,
+                auto_bounds=(_NAN, _NAN),
+                fit_auto=False,
+                method_used=models.WhittakerSmoothMethods.FIXED,
+                log_auto_bounds=(_NAN, _NAN),
+            ),
+        ),
+        (  # Number 4 (virtually coinciding floats; fixed method)
             (100.0, 100.000001),
             _FIXED_WHITTAKER_METHODS,
             ExpectedWhittakerSmoothLambda(
@@ -88,7 +106,7 @@ _all_whittaker_methods: _WhittakerMethodSequence = (
                 log_auto_bounds=(_NAN, _NAN),
             ),
         ),
-        (  # Number 3
+        (  # Number 5 (virtually coinciding floats; automated methods)
             (100.0, 100.000001),
             _aauto_whittaker_methods,
             ExpectedWhittakerSmoothLambda(
@@ -99,7 +117,7 @@ _all_whittaker_methods: _WhittakerMethodSequence = (
                 log_auto_bounds=(_NAN, _NAN),
             ),
         ),
-        (  # Number 4
+        (  # Number 6 (flipped virtually coinciding floats; fixed method)
             (100.000001, 100.0),
             _FIXED_WHITTAKER_METHODS,
             ExpectedWhittakerSmoothLambda(
@@ -110,7 +128,7 @@ _all_whittaker_methods: _WhittakerMethodSequence = (
                 log_auto_bounds=(_NAN, _NAN),
             ),
         ),
-        (  # Number 5
+        (  # Number 7 (flipped virtually coinciding floats; automated methods)
             (100.000001, 100.0),
             _aauto_whittaker_methods,
             ExpectedWhittakerSmoothLambda(
@@ -121,18 +139,7 @@ _all_whittaker_methods: _WhittakerMethodSequence = (
                 log_auto_bounds=(_NAN, _NAN),
             ),
         ),
-        (  # Number 6
-            (100.000001, 100.0),
-            _FIXED_WHITTAKER_METHODS,
-            ExpectedWhittakerSmoothLambda(
-                fixed_lambda=100.000001,
-                auto_bounds=(_NAN, _NAN),
-                fit_auto=False,
-                method_used=models.WhittakerSmoothMethods.FIXED,
-                log_auto_bounds=(_NAN, _NAN),
-            ),
-        ),
-        (  # Number 7
+        (  # Number 8 (search space floats; logml method)
             (100.0, 10_000.0),
             _LOGML_WHITTAKER_METHODS,
             ExpectedWhittakerSmoothLambda(
@@ -143,7 +150,18 @@ _all_whittaker_methods: _WhittakerMethodSequence = (
                 log_auto_bounds=(np.log(100.0), np.log(10_000.0)),
             ),
         ),
-        (  # Number 8
+        (  # Number 9 (search space integers; logml method)
+            (100, 10_000),
+            _LOGML_WHITTAKER_METHODS,
+            ExpectedWhittakerSmoothLambda(
+                fixed_lambda=_NAN,
+                auto_bounds=(100.0, 10_000.0),
+                fit_auto=True,
+                method_used=models.WhittakerSmoothMethods.LOGML,
+                log_auto_bounds=(np.log(100.0), np.log(10_000.0)),
+            ),
+        ),
+        (  # Number 10 (flipped search space floats; logml method)
             (10_000.0, 100.0),
             _LOGML_WHITTAKER_METHODS,
             ExpectedWhittakerSmoothLambda(
@@ -154,43 +172,91 @@ _all_whittaker_methods: _WhittakerMethodSequence = (
                 log_auto_bounds=(np.log(100.0), np.log(10_000.0)),
             ),
         ),
-        (  # Number 9
+        (  # Number 11 (flipped search space integers; logml method)
+            (10_000, 100),
+            _LOGML_WHITTAKER_METHODS,
+            ExpectedWhittakerSmoothLambda(
+                fixed_lambda=_NAN,
+                auto_bounds=(100.0, 10_000.0),
+                fit_auto=True,
+                method_used=models.WhittakerSmoothMethods.LOGML,
+                log_auto_bounds=(np.log(100.0), np.log(10_000.0)),
+            ),
+        ),
+        (  # Number 12 (fixed zero float; fixed method)
             0.0,
             _FIXED_WHITTAKER_METHODS,
             ValueError,
         ),
-        (  # Number 10
+        (  # Number 13 (fixed zero integer; fixed method)
+            0,
+            _FIXED_WHITTAKER_METHODS,
+            ValueError,
+        ),
+        (  # Number 14 (search space floats; fixed method)
             (100.0, 10_000.0),
             _FIXED_WHITTAKER_METHODS,
             ValueError,
         ),
-        (  # Number 11
+        (
+            # Number 15 (search space integers; fixed method)
+            (100, 10_000),
+            _FIXED_WHITTAKER_METHODS,
+            ValueError,
+        ),
+        (  # Number 16 (fixed float; automated method)
             100.0,
             _aauto_whittaker_methods,
             ValueError,
         ),
-        (  # Number 12
+        (
+            # Number 17 (fixed integer; automated method)
+            100,
+            _aauto_whittaker_methods,
+            ValueError,
+        ),
+        (  # Number 18 (search space floats with zero; all methods)
             (0.0, 100.0),
             _all_whittaker_methods,
             ValueError,
         ),
-        (  # Number 13
+        (  # Number 19 (search space integers with zero; all methods)
+            (0, 100),
+            _all_whittaker_methods,
+            ValueError,
+        ),
+        (  # Number 20 (flipped search space floats with zero; all methods)
             (100.0, 0.0),
             _all_whittaker_methods,
             ValueError,
         ),
-        (  # Number 14
+        (  # Number 21 (flipped search space integer with zero; all methods)
+            (100, 0),
+            _all_whittaker_methods,
+            ValueError,
+        ),
+        (  # Number 22 (all float zeros; all methods)
             (0.0, 0.0),
             _all_whittaker_methods,
             ValueError,
         ),
-        (  # Number 15
+        (  # Number 23 (all float integers; all methods)
+            (0, 0),
+            _all_whittaker_methods,
+            ValueError,
+        ),
+        (  # Number 24 (wrong type; all methods)
             "error",
             _all_whittaker_methods,
             TypeError,
         ),
-        (  # Number 16
+        (  # Number 25 (fixed float; wrong method)
             100.0,
+            "error",
+            ValueError,
+        ),
+        (  # Number 26 (fixed integer; wrong method)
+            100,
             "error",
             ValueError,
         ),
@@ -200,6 +266,13 @@ def test_whittaker_smooth_lambda_model(combination: _LambdaTestCombination) -> N
     """
     Tests the class :class:`WhittakerSmoothLambda` for the correct behavior of its
     ``__post_init__`` method.
+
+    The ``combination`` parameter defines
+
+    - the lambda value(s) to be used,
+    - the method(s) to be used, and
+    - the expected result(s) of the instantiation (will be an exception if the input
+        should be considered invalid by the dataclass).
 
     """
 
