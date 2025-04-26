f3308b0ab79f81b1807e14ff74d6e3a947154490
Pau Cabaneros <pau.cabaneros@gmail.com>
2025-04-26 21:33:35 +0200

    add tests to the VIP and SR

--- chemotools/feature_selection/__init__.py
+++ chemotools/feature_selection/__init__.py
@@ -1,4 +1,6 @@
 from ._index_selector import IndexSelector
 from ._range_cut import RangeCut
+from ._sr_selector import SRSelector
+from ._vip_selector import VIPSelector
 
-__all__ = ["IndexSelector", "RangeCut"]
+__all__ = ["IndexSelector", "RangeCut", "SRSelector", "VIPSelector"]
