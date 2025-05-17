c407c3a100a088da68e9f8b04e9dc48744d204f7
Pau Cabaneros <pau.cabaneros@gmail.com>
2025-05-17 17:48:46 +0200

    update ss calculation using the l2norm(X)**2

--- chemotools/feature_selection/_sr_selector.py
+++ chemotools/feature_selection/_sr_selector.py
@@ -125,8 +125,10 @@ class SRSelector(_PLSFeatureSelectorBase):
         X_hat = ttp @ ptp.T
 
         # Compute squared norms directly
-        total_ss = np.sum(X**2, axis=0)
-        explained_ss = np.sum(X_hat**2, axis=0)
+        total_ss = np.linalg.norm(X, axis=0)**2
+        explained_ss = np.linalg.norm(X_hat, axis=0)**2
+
+        # Calculate residual sum of squares
         residual_ss = total_ss - explained_ss
 
         # Stability: avoid division by zero
