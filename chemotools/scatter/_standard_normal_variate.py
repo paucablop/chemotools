678be1a0f323d3421a1343851ce45daa28963aa2
Pau Cabaneros <pau.cabaneros@gmail.com>
2025-09-20 13:12:37 +0200

    chore: remove reducndant input validation

--- chemotools/scatter/_standard_normal_variate.py
+++ chemotools/scatter/_standard_normal_variate.py
@@ -70,12 +70,6 @@ class StandardNormalVariate(TransformerMixin, OneToOneFeatureMixin, BaseEstimato
             dtype=np.float64,
         )
 
-        # Check that the number of features is the same as the fitted data
-        if X_.shape[1] != self.n_features_in_:
-            raise ValueError(
-                f"Expected {self.n_features_in_} features but got {X_.shape[1]}"
-            )
-
         # Calculate the standard normal variate
         for i, x in enumerate(X_):
             X_[i] = self._calculate_standard_normal_variate(x)
