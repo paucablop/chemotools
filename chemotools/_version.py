dcf197fa7ee3d7ca95471df15c769e0d6eab6cae
paucablop <pau.cabaneros@gmail.com>
2026-02-18 01:10:00 +0800

    fix: exclude generated _version.py file from format check

--- chemotools/_version.py
+++ chemotools/_version.py
@@ -12,7 +12,8 @@ __all__ = [
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
-    from typing import Tuple, Union
+    from typing import Tuple
+    from typing import Union
 
     VERSION_TUPLE = Tuple[Union[int, str], ...]
     COMMIT_ID = Union[str, None]
@@ -27,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.2.6.dev26+g5e8b67750.d20260217"
-__version_tuple__ = version_tuple = (0, 2, 6, "dev26", "g5e8b67750.d20260217")
+__version__ = version = '0.2.6.dev27+gbeb84a8e2.d20260217'
+__version_tuple__ = version_tuple = (0, 2, 6, 'dev27', 'gbeb84a8e2.d20260217')
 
 __commit_id__ = commit_id = None
