c775a32370abab5b58de4c1196f0caba90dd15e4
Ruggero Guerrini <gr6898@gmail.com>
2026-04-14 14:42:29 +0200

    feat: initial implementation of Direct Standardization

--- chemotools/domain_adaption/_direct_standardization.py
+++ chemotools/domain_adaption/_direct_standardization.py
@@ -12,7 +12,12 @@ from sklearn.base import BaseEstimator, TransformerMixin
 class DirectStandardization(BaseEstimator, TransformerMixin):
     """
     Description
-    Implement a Direct Standardization transformer for calibration transfer application
+    -----------
+    Implement a direct standardization transformer for the calibration transfer application.
+    X_master contains the reference measurements acquired on the master instrument.
+    X_slave contains the corresponding measurements of the same samples acquired on the slave instrument.
+    The transformer estimates a mapping from the slave space to the master space.
+    After fitting, new X_slave spectra can be transformed into the X_master space.
 
     Parameters
     ----------
@@ -21,51 +26,53 @@ class DirectStandardization(BaseEstimator, TransformerMixin):
     Attributes
     ----------
     T : np.ndarray of shape (n_features, n_features)
-        The pxp matrix that solver the problem X_bench T = X_portable
+        The pxp matrix that solver the problem X_slave T = X_master
         using the method of least squares
 
     Examples
     --------
-        X_bench = np.random.randn((100,50))
-        X_portbale = X_bench*2+5
-        DS = DirectStandardization().fit(X_bench,X_portable)
-        X_bench_transf = DS.transform(X_bench)
-    
+        X_slave = np.random.randn((100,50))
+        X_portbale = X_slave*2+5
+        DS = DirectStandardization().fit(X_slave,X_master)
+        X_slave_transf = DS.transform(X_slave)
+
     """
+
     def __init__(self):
         pass
-    def fit(self, X_bench: np.ndarray, X_portable: np.ndarray) -> DirectStandardization:
+
+    def fit(self, X_slave: np.ndarray, X_master: np.ndarray) -> DirectStandardization:
         """
         Fit the DirectStandardization to the input data.
 
         Parameters
         ----------
-        X_bench : np.ndarray of shape (n_samples, n_features)
-            The bench data
-        X_portable : np.ndarray of shape (n_samples, n_features)
-            The portable data
+        X_slave : np.ndarray of shape (n_samples, n_features)
+            The slave data
+        X_master : np.ndarray of shape (n_samples, n_features)
+            The master data
 
         Returns
         -------
         self : DirectStandardization
             The fitted model.
         """
-        X_bench = np.asarray(X_bench, dtype=float)
-        X_portable = np.asarray(X_portable, dtype=float)
-        if X_portable.shape != X_bench.shape:
-            raise ValueError ("Portable and bench must have the same dimensions")
-        self.T = np.linalg.pinv(X_bench) @ X_portable
+        X_slave = np.asarray(X_slave, dtype=float)
+        X_master = np.asarray(X_master, dtype=float)
+        if X_master.shape != X_slave.shape:
+            raise ValueError("master and slave must have the same dimensions")
+        self.T = np.linalg.pinv(X_slave) @ X_master
         return self
-    def transform(self,X_bench) -> np.ndarray:
+
+    def transform(self, X_slave) -> np.ndarray:
         """
-        Fit the Direct standardization transforme and 
-        transform the bench data 
+        Transform the slave data
 
         Parameters
         ----------
-        X+bench : np.ndarray of shape (n_samples, n_features)
+        X_slave : np.ndarray of shape (n_samples, n_features)
             The input data to transform
-        
+
         Returns
         -------
         X_transf : np.ndarray of shape (n_samples, n_features)
@@ -73,4 +80,4 @@ class DirectStandardization(BaseEstimator, TransformerMixin):
         """
         if self.T is None:
             raise RuntimeError("Model not trained. Call train() first.")
-        return X_bench @ self.T 
+        return X_slave @ self.T
