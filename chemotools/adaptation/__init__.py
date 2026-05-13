8f1990443be3ad17c87c62b8a36a53c2531fbd46
paucablop <pau.cabaneros@gmail.com>
2026-05-13 14:29:21 +0800

    fix: (adaptation) add module level init

---
+++ chemotools/adaptation/__init__.py
@@ -0,0 +1,7 @@
+from ._direct_standardization import DirectStandardization
+from ._piecewise_direct_standardization import PiecewiseDirectStandardization
+
+__all__ = [
+    "DirectStandardization",
+    "PiecewiseDirectStandardization"
+]
