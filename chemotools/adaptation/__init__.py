111582b14932581e0b36eb6636546c684bfb5982
paucablop <pau.cabaneros@gmail.com>
2026-05-13 22:03:27 +0800

    fix: (adaptation) format __init__ file

--- chemotools/adaptation/__init__.py
+++ chemotools/adaptation/__init__.py
@@ -2,4 +2,8 @@ from ._direct_standardization import DirectStandardization
 from ._piecewise_direct_standardization import PiecewiseDirectStandardization
 from ._x_axis_interpolator import XAxisInterpolator
 
-__all__ = ["DirectStandardization", "PiecewiseDirectStandardization", "XAxisInterpolator"]
+__all__ = [
+    "DirectStandardization",
+    "PiecewiseDirectStandardization",
+    "XAxisInterpolator",
+]
