7fc5b96a860ff35e9575d656f33a8680c2d3d336
paucablop <pau.cabaneros@gmail.com>
2026-05-13 14:29:47 +0800

    refactor: (adaptation) format module level init

--- chemotools/adaptation/__init__.py
+++ chemotools/adaptation/__init__.py
@@ -1,7 +1,4 @@
 from ._direct_standardization import DirectStandardization
 from ._piecewise_direct_standardization import PiecewiseDirectStandardization
 
-__all__ = [
-    "DirectStandardization",
-    "PiecewiseDirectStandardization"
-]
+__all__ = ["DirectStandardization", "PiecewiseDirectStandardization"]
