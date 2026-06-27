3317b5ad26f7f4796cf145e2de0578d86acd76f7
paucablop <pau.cabaneros@gmail.com>
2026-06-28 01:13:57 +0800

    feat(adaptation:MetadataFunctionTransformer): expose method in init

--- chemotools/adaptation/__init__.py
+++ chemotools/adaptation/__init__.py
@@ -1,11 +1,15 @@
+from . import functions
 from ._direct_standardization import DirectStandardization
+from ._metadata_function_transformer import MetadataFunctionTransformer
 from ._piecewise_direct_standardization import PiecewiseDirectStandardization
 from ._spectral_space_transform import SpectralSpaceTransform
 from ._x_axis_interpolator import XAxisInterpolator
 
 __all__ = [
     "DirectStandardization",
+    "MetadataFunctionTransformer",
     "PiecewiseDirectStandardization",
     "SpectralSpaceTransform",
     "XAxisInterpolator",
+    "functions",
 ]
